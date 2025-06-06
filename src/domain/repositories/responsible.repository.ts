3f4fdd1d7b20ec90ef30900ce49a74998220cc91
Igor Pardinho <pardinhoigor@gmail.com>
2025-06-06 20:10:52 -0300

    feat: cria o repository do responsible no domain

---
+++ src/domain/repositories/responsible.repository.ts
@@ -0,0 +1,8 @@
+import { Responsible } from './../entities/responsible';
+
+export abstract class ResponsibleRepository {
+  abstract findAll(): Promise<Responsible[]>;
+  abstract create(responsible: Responsible): Promise<Responsible>;
+  abstract findOne(id: string): Promise<Responsible | null>;
+  abstract delete(id: string): Promise<void>;
+}
