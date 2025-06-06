4b28a5bcc52528d958b3d05c092895e480e36632
Igor Pardinho <pardinhoigor@gmail.com>
2025-06-06 19:52:31 -0300

    feat: cria o repository do product no domain

--- src/domain/repositories/company.repository.ts
+++ src/domain/repositories/company.repository.ts
@@ -2,7 +2,7 @@ import { Company } from '../entities/company';
 
 export abstract class CompanyRepository {
   abstract findAll(): Promise<Company[]>;
-  abstract create(): Promise<void>;
+  abstract create(company: Company): Promise<void>;
   abstract findOne(id: string): Promise<Company | null>;
   abstract delete(id: string): Promise<void>;
 }
