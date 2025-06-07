c054fa93a9ab91a2779ea5915214a59daf785cbe
Igor Pardinho <pardinhoigor@gmail.com>
2025-06-06 21:59:16 -0300

    feat: cria o usecase create do product no application

---
+++ src/application/usecases/create-product.ts
@@ -0,0 +1,10 @@
+import { Product } from '../../domain/entities/product';
+import { ProductRepository } from '../../domain/repositories/product.repository';
+
+export class CreateProduct {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async execute(product: Product): Promise<void> {
+    return await this.productRepository.create(product);
+  }
+}
