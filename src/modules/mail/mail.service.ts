30503ac3517473052bcfe97f5f3e28d196169b9b
Guilherme Artigas <guilherme.artigas92@gmail.com>
2025-08-16 18:11:58 -0300

    refactor: alterando atribuicao de seguranca para envio de emails

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -13,7 +13,7 @@ export class MailService {
     this.transporter = nodemailer.createTransport({
       host: this.configService.get<string>('MAIL_HOST'),
       port: this.configService.get<number>('MAIL_PORT'),
-      secure: this.configService.get<boolean>('MAIL_SECURE'),
+      secure: this.configService.get<string>('MAIL_SECURE').includes('true'),
       auth: {
         user: this.configService.get<string>('MAIL_USER'),
         pass: this.configService.get<string>('MAIL_PASSWORD'),
